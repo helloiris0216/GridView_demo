91ff42685fc8ce5622bf801a0da2918d074b25a1
helloiris0216 <l66074106>
2020-11-07 10:45:29 +0800

    Feat:sorted out notes
    issue #002

--- app/src/main/java/com/example/gridview/MainActivity.java
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
     private List<Map<String, Object>> itemList;
 
+
+    //在onCreate()內呼叫方法
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         myGridViewAdaptor();
     }
 
+    //找到元件
     private void findViews(){
 
         gridViewPic = (GridView) findViewById(R.id.gridView_pic);
@@ -46,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //打包:將資料()先放到itemData內
+    //使用Map打包:先將資料(image & name)放到itemData內
     //再將itemData放到itemList內
     private void putDataIntoList(){
 
@@ -61,6 +64,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //將資料顯示在畫面上(activity_main.xml)
+    /*
+     1.先new出adaptor:line 76
+     2.建立外部類別MyGridAdaptor
+     3.接著邏輯會跑到MyGridAdaptor.java
+     4.在外部類別MyGridAdaptor設定要將資料顯示在哪個layout，以及取出資料
+     5.將adaptor放入gridView中:line78
+     6.監聽girdView
+     7.(不確定)item:將資料透過Position取出:line 84
+     */
     private void myGridViewAdaptor(){
 
         MyGridAdaptor adaptor = new MyGridAdaptor(context, itemList);
--- app/src/main/java/com/example/gridview/MyGridAdaptor.java
+++ app/src/main/java/com/example/gridview/MyGridAdaptor.java
@@ -11,43 +11,64 @@ import android.widget.TextView;
 import java.util.List;
 import java.util.Map;
 
+
+//在外部類別MyGridAdaptor設定要將資料顯示在哪個layout，以及取出資料
 public class MyGridAdaptor extends BaseAdapter {
     private final LayoutInflater myLayoutInflater;
     private final List<Map<String, Object>> myItemList;
 
+
+    //建構子:傳入Context, Map, itemList
+    //Context:是哪個activity
+    //myItemList = itemList:內有image, name
     public MyGridAdaptor(Context context, List<Map<String, Object>> itemList) {
         myLayoutInflater = LayoutInflater.from(context);
         myItemList = itemList;
     }
 
+
+    //getCount():有多少欄位
     @Override
     public int getCount() {
         return myItemList.size();
     }
 
+    //getItem():透過取得item的Position來取得item
+    //Position:myItemList的元素位置=0,1,2,3,4,5,6,7,8
+    //每個Position內含有貼兩種標籤的元素各一個:image & name
     @Override
     public Object getItem(int position) {
         return myItemList.get(position);
     }
 
+    //getItemId():透過取得Position來取得item的id
     @Override
     public long getItemId(int position) {
         return position;
     }
 
-
+    //建立內部類別 ViewHolder:為了存取資料
+    //內有兩個屬性:imageId & nameId
     private class ViewHolder{
         ImageView imageId;
         TextView nameId;
     }
 
+    //為了將取出的資料顯示在item_layout中
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         ViewHolder holder;
 
-        //存id
-        //如果convertView是空的
+        //if..else:存id用
+        /*
+		 1.如果convertView是空的:
+         2.就顯示item_layout
+         3.建立ViewHolder holder
+         4.找到在item_layout中的imageView_id，並存到imageId中
+         5.找到在item_layout中的textView_name，並存到imageId中
+         6.將holder放入convertView內
+		*/
         if (convertView == null){
             convertView = myLayoutInflater.inflate(R.layout.item_layout, null);
             holder = new ViewHolder();
@@ -60,6 +81,7 @@ public class MyGridAdaptor extends BaseAdapter {
         }
 
         //將資料(myItemList)從Map(item)中取出來
+        //因為每個Position內的貼有標籤(image & name)的元素只有各一個，所以不需要用到陣列來取出
         Map<String, Object> item = myItemList.get(position);
         int number = (int) item.get("image");
         String data = (String) item.get("name");
